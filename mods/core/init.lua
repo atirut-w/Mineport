793cfd768cd48a745b2b4c6cb4831e703b58412b
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-11-12 22:44:46 +0700

    Add some HUD textures

--- mods/core/init.lua
+++ mods/core/init.lua
@@ -1,6 +1,12 @@
 ---@diagnostic disable-next-line: lowercase-global
 mineport = {}
 
+minetest.register_on_joinplayer(function(obj, last_login)
+    obj:hud_set_hotbar_itemcount(9)
+    obj:hud_set_hotbar_image("mineport_hotbar.png")
+    obj:hud_set_hotbar_selected_image("mineport_hotbar_selection.png")
+end)
+
 minetest.register_node(":mineport:stone", {
     tiles = {"mineport_stone.png"},
 })
