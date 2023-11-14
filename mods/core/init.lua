fe6909702830f51be7dbf12594a8d043d0ad2cf1
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-11-14 16:41:31 +0700

    Wrap running scripts in `do ... end`

--- mods/core/init.lua
+++ mods/core/init.lua
@@ -8,8 +8,10 @@ function mineport.get_current_modpath()
     return minetest.get_modpath(minetest.get_current_modname())
 end
 
-local modpath = mineport.get_current_modpath()
-dofile(modpath .. "/callbacks.lua")
+do
+    local modpath = mineport.get_current_modpath()
+    dofile(modpath .. "/callbacks.lua")
+end
 
 minetest.register_node(":mineport:stone", {
     tiles = {"mineport_stone.png"},
